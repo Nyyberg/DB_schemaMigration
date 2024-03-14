cd24ddc1beae6d48957405dc35d48d0b452383ba
Nyyberg <92079143+Nyyberg@users.noreply.github.com>
2024-03-14 19:55:26 +0100

    initial setup

---
+++ DBMigrationEF/Models/Models.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+
+public class StoreContext : DbContext
+{
+    public DbSet<Products> Blogs { get; set; }
+   
+
+    public string DbPath { get; }
+
+    public StoreContext()
+    {
+        var folder = Environment.SpecialFolder.LocalApplicationData;
+        var path = Environment.GetFolderPath(folder);
+        DbPath = System.IO.Path.Join(path, "Store.db");
+    }
+
+    // The following configures EF to create a Sqlite database file in the
+    // special "local" folder for your platform.
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite($"Data Source={DbPath}");
+}
+
+public class Products
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public float Price { get; set; }
+}
